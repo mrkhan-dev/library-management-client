b5ff6f675f64689d2992d5e2fef92e5c3a6f6d06
mrkhan-dev <sk5094625@gmail.com>
2024-05-10 20:21:03 +0600

    using helmet provider for dynamic title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import {Helmet} from "react-helmet-async";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Home | ShelfMaster</title>
+      </Helmet>
       <h1 className="text-7xl text-center ">HomePage</h1>
     </div>
   );
